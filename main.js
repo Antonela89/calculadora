77e96dd93cdc6802050de2cec9debd6193dc60ef
Antonela <antonelaborgogno@gmail.com>
2023-09-17 17:13:03 -0300

    cambios en js y logica

--- main.js
+++ main.js
@@ -1,25 +1,54 @@
+// Seleccionar elementos del DOM
+const expresionElement = document.querySelector(".expresion");
+const resultadoElement = document.querySelector(".resultado");
 const botones = document.querySelectorAll(".boton");
-const expresion = document.querySelector(".expresion");
-const resultado = document.querySelector(".resultado");
+// const botonIgual = document.querySelector(".boton-igual");
+// const botonReceteo = document.querySelector(".boton-receteo");
 
-// console.log(botones);
-// console.log(expresion);
+// console.log(Math);
 
+let ultimoBotonIgual = false; // Inicialmente, el último botón no fue el de igual
+
+// Manejar eventos de los botones
 botones.forEach((boton) => {
-    boton.addEventListener("click", (e) => {
-        console.log(typeof(e.target.innerHTML));//string
-        if(e.target.innerHTML === "AC") {
-            expresion.innerHTML = "";
+    boton.addEventListener("click", () => {
+        if (ultimoBotonIgual) {
+            // Si el último botón fue el de igual, limpiar la expresión
+            expresionElement.textContent = "";
+            resultadoElement.textContent = "";
+            ultimoBotonIgual = false; // Reiniciar el estado
+        }
+
+        if (boton.classList.contains("boton-borrar")) {
+            const nuevaExpresion = expresionElement.textContent.slice(0, -1);
+            expresionElement.textContent = nuevaExpresion;
+            ultimoBotonIgual = false; // Reiniciar el estado
         } else {
-            let expresion_parcial = e.target.innerHTML;
-            expresion.innerHTML += expresion_parcial;
-            // if(expresion.innerHTML !== "()" || "%" || "/" || "*"  || "-" || "+" || "=" ) {
-            //     let valor = Number(expresion.innerHTML);
-            //     console.log(valor);
-            // } else {
-            //     valor = "";
-            // }
-            console.log(typeof(expresion.innerHTML)); //string
+            expresionElement.textContent += boton.textContent; // agrega la expresión matemática
+        }
+
+        if(boton.classList.contains("boton-igual")) {
+            try {
+                const expresion = expresionElement.textContent.slice(0, -1);
+                // console.log(expresion);
+                const resultado = math.evaluate(expresion);
+                // console.log(resultado);
+                resultadoElement.textContent = resultado;
+                ultimoBotonIgual = true; // El último botón fue el de igual
+            } catch (error) {
+                resultadoElement.textContent = "Error";
+            }
+        }
+
+        if (boton.classList.contains("boton-receteo")) {
+            expresionElement.textContent = "";
+            resultadoElement.textContent = "";
+            ultimoBotonIgual = false; // Reiniciar el estado
         }
     });
 });
+
+//colocar función englobadora del código para asegurar qe se ejecute despues de q se cargue todo el código
+document.addEventListener("DOMContentLoaded", function () {
+    // Coloca aquí función
+})
